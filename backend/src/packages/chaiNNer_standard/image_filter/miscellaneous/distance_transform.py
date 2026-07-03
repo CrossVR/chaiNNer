7a2a50bb2f78b93eda0637663a1e6e965378cd71
Jules Blok <jules.blok@gmail.com>
2026-07-04 01:26:02 +0900

    distance_transform: Add more algorithms and cut-off parameter

--- backend/src/packages/chaiNNer_standard/image_filter/miscellaneous/distance_transform.py
+++ backend/src/packages/chaiNNer_standard/image_filter/miscellaneous/distance_transform.py
@@ -1,17 +1,26 @@
 from __future__ import annotations
 
+from enum import Enum
+
 import cv2
 import numpy as np
-from chainner_ext import esdf
+from chainner_ext import esdf, edtaa
 
 from nodes.impl.image_utils import as_3d, to_uint8
-from nodes.properties.inputs import BoolInput, ImageInput, NumberInput
+from nodes.properties.inputs import EnumInput, ImageInput, NumberInput, SliderInput
 from nodes.properties.outputs import ImageOutput
 
 from .. import miscellaneous_group
 
 
-def binary_sdf(img: np.ndarray, spread: float) -> np.ndarray:
+class DistanceAlgorithm(Enum):
+    BINARY = 1
+    ESDF = 2
+    EDTAA3 = 3
+    EDTAA4 = 4
+
+
+def binary_sdf(img: np.ndarray, spread: float, cutoff: float) -> np.ndarray:
     img = as_3d(to_uint8(img, normalized=True))
     img[img < 128] = 0
     img[img >= 128] = 255
@@ -37,8 +46,8 @@ def binary_sdf(img: np.ndarray, spread: float) -> np.ndarray:
     img1 = img.ravel()
     signed_distance = np.empty(shape=img.shape, dtype=np.float32).ravel()
 
-    signed_distance[img1 == 255] = black_dist.ravel()[img1 == 255] / spread / 2 + 0.5
-    signed_distance[img1 == 0] = 0.5 - white_dist.ravel()[img1 == 0] / spread / 2
+    signed_distance[img1 == 255] = black_dist.ravel()[img1 == 255] / spread / 2 + cutoff
+    signed_distance[img1 == 0] = cutoff - white_dist.ravel()[img1 == 0] / spread / 2
 
     signed_distance = np.clip(signed_distance, 0, 1)
 
@@ -53,9 +62,15 @@ def binary_sdf(img: np.ndarray, spread: float) -> np.ndarray:
     inputs=[
         ImageInput(channels=1),
         NumberInput("Spread", min=1, default=4),
-        BoolInput("Sub-pixel precision", default=False).with_docs(
-            "If enabled, then anti-aliasing will be accounted for. If not enabled, then the image will be converted to binary (either black or white) before processing.",
-            "Enabling this option will significantly improve the results of anti-aliased shapes, but it cannot be used on anything else. It assumes strictly binary shapes (with optional anti-aliasing), and will return incorrect results for e.g. blurry images. If you cannot guarantee binary image, use the `chainner:image:threshold` node with *Anti-aliasing* enabled.",
+        SliderInput("Cutoff", min=0, default=0.5, max=1, precision=2),
+        EnumInput(DistanceAlgorithm, option_labels={
+            DistanceAlgorithm.ESDF: 'ESDF',
+            DistanceAlgorithm.EDTAA3: 'EDTAA3',
+            DistanceAlgorithm.EDTAA4: 'EDTAA4',
+        }
+        ).with_docs(
+            "If Binary is selected, then the image will be converted to binary (either black or white) before processing.",
+            "Choosing ESDF or EDTAA will significantly improve the results of anti-aliased shapes, but it cannot be used on anything else. It assumes strictly binary shapes (with optional anti-aliasing), and will return incorrect results for e.g. blurry images. If you cannot guarantee binary image, use the `chainner:image:threshold` node with *Anti-aliasing* enabled.",
             "Sub-pixel distance transform is implemented using the excellent [ESDF algorithm](https://acko.net/blog/subpixel-distance-transform/) by Steven Wittens.",
         ),
     ],
@@ -64,8 +79,14 @@ def binary_sdf(img: np.ndarray, spread: float) -> np.ndarray:
 def distance_transform_node(
     img: np.ndarray,
     spread: int,
-    use_esdf: bool,
+    cutoff: float,
+    algorithm: DistanceAlgorithm,
 ) -> np.ndarray:
-    if use_esdf:
-        return esdf(img, spread * 2, 0.5, False, True)
-    return binary_sdf(img, spread)
+    if algorithm == DistanceAlgorithm.ESDF:
+        return esdf(img, spread * 2, cutoff, False, True)
+    if algorithm == DistanceAlgorithm.EDTAA3:
+        return edtaa(img, spread * 2, cutoff, True, False)
+    if algorithm == DistanceAlgorithm.EDTAA4:
+        return edtaa(img, spread * 2, cutoff, False, True)
+    else:
+        return binary_sdf(img, spread, cutoff)
