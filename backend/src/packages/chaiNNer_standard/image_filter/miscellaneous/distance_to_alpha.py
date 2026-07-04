d6e181d835bfeff58b445e829c32a6f28115fa99
Jules Blok <jules.blok@gmail.com>
2026-07-04 14:48:03 +0900

    image_filter: Add new distance-to-alpha conversion node

---
+++ backend/src/packages/chaiNNer_standard/image_filter/miscellaneous/distance_to_alpha.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import numpy as np
+
+from nodes.properties.inputs import ImageInput, NumberInput, SliderInput
+from nodes.properties.outputs import ImageOutput
+
+from .. import miscellaneous_group
+
+
+@miscellaneous_group.register(
+    schema_id="chainner:image:distance_to_alpha",
+    name="Distance to Alpha",
+    description="Converts a distance field to an alpha mask.",
+    icon="MdBlurOn",
+    inputs=[
+        ImageInput(channels=1),
+        NumberInput("Spread", min=1, default=4),
+        SliderInput("Cutoff", min=0, default=0.5, max=1, precision=2),
+        SliderInput("Edge Width", min=0.01, default=0.5, max=4, precision=2),
+    ],
+    outputs=[ImageOutput(shape_as=0, assume_normalized=True)],
+)
+def distance_to_alpha_node(
+    img: np.ndarray,
+    spread: int,
+    cutoff: float,
+    width: float,
+) -> np.ndarray:
+    # This is essentially a pivoted contrast node
+    contrast = (spread * 2) / width
+    pivot = 1 - cutoff
+    return np.clip(pivot + (img - pivot) * contrast, 0, 1)
